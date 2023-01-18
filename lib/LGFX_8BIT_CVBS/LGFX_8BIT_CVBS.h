41449f8f2dbd4a5775684ed4f359d62908c7f80e
riraosan <fiatpanda750@icloud.com>
2023-01-18 15:16:21 +0900

    for center

--- lib/LGFX_8BIT_CVBS/LGFX_8BIT_CVBS.h
+++ lib/LGFX_8BIT_CVBS/LGFX_8BIT_CVBS.h
@@ -16,11 +16,11 @@ public:
       cfg.memory_height = 240;  // 出力解像度 高さ
 
       // 実際に利用する解像度を設定;
-      cfg.panel_width  = 360;       // 実際に使用する幅   (memory_width と同値か小さい値を設定する)
+      cfg.panel_width  = 360 - 8;       // 実際に使用する幅   (memory_width と同値か小さい値を設定する)
       cfg.panel_height = 240 - 16;  // 実際に使用する高さ (memory_heightと同値か小さい値を設定する)
 
       // 表示位置オフセット量を設定;
-      cfg.offset_x = 0;  // 表示位置を右にずらす量 (初期値 0)
+      cfg.offset_x = 4;  // 表示位置を右にずらす量 (初期値 0)
       cfg.offset_y = 8;  // 表示位置を下にずらす量 (初期値 0)
 
       _panel_instance.config(cfg);
